19804c6d596d29aff4ec17d9808d499b75289c38
Ryzzen <a.mayaud@hotmail.fr>
2023-12-31 19:57:14 +0100

    [FIX](dwt delay) Registers now always reset

--- Core/Src/dwt_delay.c
+++ Core/Src/dwt_delay.c
@@ -3,13 +3,11 @@
 
 void DWT_Init(void)
 {
-    if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) {
+    /* if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) { */
         CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
         DWT->CYCCNT = 0;
         DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
-		__BKPT();
-
-    }
+    /* } */
 }
 
 void DWT_Delay(uint32_t us)
